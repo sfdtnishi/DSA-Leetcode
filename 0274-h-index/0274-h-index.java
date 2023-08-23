25836bfdf7212b1681feb910ffd2d09300a52ea7
Nishi Jain <79569723+sfdtnishi@users.noreply.github.com>
2023-08-23 13:48:38 +0530

    Time: 2 ms (64.40%), Space: 40.2 MB (77.87%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int ans=0;
+        int n= citations.length;
+        for(int i=0;i<n;i++){
+            if((n-i)<=citations[i]){
+            ans=n-i;
+            break;
+            }
+        }
+        return ans;
+    }
+}
